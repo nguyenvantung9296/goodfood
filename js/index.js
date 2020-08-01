393daef9a9f4e2a6fd67b3bd34126c3d43242d57
nguyenvantung9296 <tungnguyenvan9296@gmail.com>
2020-08-01 12:22:33 +0700

    update-banner-foodgood

--- js/index.js
+++ js/index.js
@@ -22,3 +22,34 @@ $(function () {
             
         });
     })
+//IMAGE
+    var slideshow = document.getElementById('slideShow');
+    var slides = slideshow.getElementsByTagName('img')
+    var index = 0;
+    
+    function nextSlide(){
+        slides[index].classList.remove('active-image');
+        index = (index + 1) % slides.length
+        slides[index].classList.add('active-image');
+    }
+    function backSlide(){
+        slides[index].classList.remove('active-image');
+        index = (index - 1 + slides.length) % slides.length
+        slides[index].classList.add('active-image');
+    }
+
+//CONTENT   
+    var slideshowText = document.getElementById('slideShowText');
+    var slidesText = slideshowText.getElementsByClassName('flex__content')
+    var i = 0;
+    
+    function nextSlideText(){
+        slidesText[i].classList.remove('content--active');
+        i = (i + 1) % slidesText.length
+        slidesText[i].classList.add('content--active');
+    }
+    function backSlideText(){
+        slidesText[i].classList.remove('content--active');
+        i = (i - 1 + slidesText.length) % slidesText.length
+        slidesText[i].classList.add('content--active');
+    }
