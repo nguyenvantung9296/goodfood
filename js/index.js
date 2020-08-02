276c71e8e953d62d7a3f14f9487953ca27ed3645
nguyenvantung9296 <tungnguyenvan9296@gmail.com>
2020-08-02 23:55:13 +0700

    update-title-banner

--- js/index.js
+++ js/index.js
@@ -25,11 +25,12 @@ $(function () {
 //IMAGE
     var slideshow = document.getElementById('slideShow');
     var slides = slideshow.getElementsByTagName('img')
+    console.log(slides.length)
     var index = 0;
     
     function nextSlide(){
         slides[index].classList.remove('active-image');
-        index = (index + 1) % slides.length
+        index = (index + 1 + slides.length) % slides.length
         slides[index].classList.add('active-image');
     }
     function backSlide(){
